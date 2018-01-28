575525b758583aa04c5b837cd8a6f180911f8705
binyaha <yahabin@gmail.com>
2018-01-28 19:02:18 +0800

    Create model:Article and 2 validates

---
+++ app/models/article.rb
@@ -0,0 +1,4 @@
+class Article <ActiveRecord::Base
+  validates :title, presence: true, length: {minimum: 1, maximum: 50}
+  validates :description, presence: true, length: {minimum: 2, maximum: 300}
+end
